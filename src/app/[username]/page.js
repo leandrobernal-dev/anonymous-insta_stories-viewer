219ffbfafc2a2ca6ec08ff823a2c9031de801a5a
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-19 10:57:06 +0800

    Center profile picture for mobile

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -25,24 +25,26 @@ export default function Home({ params }) {
                         <div className="mt-4">
                             {data && (
                                 <header className="sm:flex gap-8">
-                                    <div className="w-52 relative h-52 aspect-square p-1 rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600">
-                                        <div className="w-full h-full rounded-full overflow-hidden">
-                                            <Image
-                                                src={data.profilePic}
-                                                alt=""
-                                                width={200}
-                                                height={200}
-                                                className="w-full h-full object-cover"
-                                            />
-                                        </div>
-                                        {Number(data.storyCount) ===
-                                        0 ? null : (
-                                            <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500 rounded-full flex items-center justify-center border-2 border-white">
-                                                <span className="text-white text-sm font-black">
-                                                    {data.storyCount}
-                                                </span>
+                                    <div className="flex justify-center">
+                                        <div className="w-52 relative h-52 aspect-square p-1 rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600">
+                                            <div className="w-full h-full rounded-full overflow-hidden">
+                                                <Image
+                                                    src={data.profilePic}
+                                                    alt=""
+                                                    width={200}
+                                                    height={200}
+                                                    className="w-full h-full object-cover"
+                                                />
                                             </div>
-                                        )}
+                                            {Number(data.storyCount) ===
+                                            0 ? null : (
+                                                <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500 rounded-full flex items-center justify-center border-2 border-white">
+                                                    <span className="text-white text-sm font-black">
+                                                        {data.storyCount}
+                                                    </span>
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
                                     <div className="mt-8">
                                         <div>
