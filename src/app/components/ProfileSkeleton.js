6f0b12d11a50f1fb7d233ecbe731c0f78f333651
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-19 17:06:52 +0800

    Remove Story Count badge

--- src/app/components/ProfileSkeleton.js
+++ src/app/components/ProfileSkeleton.js
@@ -8,9 +8,6 @@ export default function ProfileSkeleton() {
                     <div>
                         <div className="w-52 h-52 relative rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600 p-1">
                             <Skeleton className="w-full h-full rounded-full bg-white" />
-                            <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500  rounded-full flex items-center justify-center border-2 border-white">
-                                <Skeleton className="w-4 h-4 rounded-full" />
-                            </div>
                         </div>
                     </div>
                 </div>
