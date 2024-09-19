889dce42a772c6359854e2cfbdf6d42db211e40f
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-19 08:56:44 +0800

    Update profile page layout

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import axios from "axios";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home({ params }) {
@@ -8,14 +9,56 @@ export default function Home({ params }) {
 
     useEffect(() => {
         axios.get(`/api/${params.username}`).then((data) => {
-            console.log(data.data);
             setData(data.data);
         });
     }, []);
     return (
-        <div>
-            {params.username}
-            {data && <video src={data.videoUrl}></video>}
-        </div>
+        <main>
+            <div className="flex w-full justify-center">
+                {data && (
+                    <div className="flex gap-8">
+                        <div className="w-64 relative h-64 aspect-square p-1 rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600">
+                            <div className="w-full h-full rounded-full overflow-hidden">
+                                <Image
+                                    src={data.profilePic}
+                                    alt=""
+                                    width={200}
+                                    height={200}
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                            {data.storyCount && (
+                                <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500 rounded-full flex items-center justify-center border-2 border-white">
+                                    <span className="text-white text-sm font-bold">
+                                        {data.storyCount}
+                                    </span>
+                                </div>
+                            )}
+                        </div>
+                        <div className="mt-8">
+                            <div className="flex gap-2">
+                                <span>
+                                    <strong>{data.posts}</strong> posts
+                                </span>
+                                <span>
+                                    <strong>{data.followers}</strong> followers
+                                </span>
+                                <span>
+                                    <strong>{data.following}</strong> following
+                                </span>
+                            </div>
+                            <h1>
+                                {data.name} <span>{data.pronoun}</span>
+                            </h1>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: data.descriptionHtml,
+                                }}
+                            ></div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </main>
     );
 }
