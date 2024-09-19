fc0aeb9acc4e3043d8bf47cb3627c268a65367d8
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-19 17:36:41 +0800

    Create: constants.js file

---
+++ src/app/constants/constants.js
@@ -0,0 +1,6 @@
+export const chromiumPack =
+    "https://github.com/Sparticuz/chromium/releases/download/v127.0.0/chromium-v127.0.0-pack.tar";
+export const loginUrl = "https://www.instagram.com/accounts/login/";
+export const loginUsername = process.env.INSTA_USERNAME;
+export const logingPass = process.env.INSTA_PASSWORD;
+export const isLocal = process.env.DEPLOYMENT === "development";
