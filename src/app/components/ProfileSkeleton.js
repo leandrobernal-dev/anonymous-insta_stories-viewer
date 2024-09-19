0eeb5c3af6c04f2fede2ece10f64ba9a28f83ab2
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-19 13:11:51 +0800

    fix profile picture skeleton

--- src/app/components/ProfileSkeleton.js
+++ src/app/components/ProfileSkeleton.js
@@ -5,10 +5,12 @@ export default function ProfileSkeleton() {
         <div className="mt-4">
             <header className="sm:flex gap-8">
                 <div className="flex justify-center">
-                    <div className="w-52 h-52 relative rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600 p-1">
-                        <Skeleton className="w-full h-full rounded-full" />
-                        <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500 rounded-full flex items-center justify-center border-2 border-white">
-                            <Skeleton className="w-4 h-4 rounded-full" />
+                    <div>
+                        <div className="w-52 h-52 relative rounded-full bg-gradient-to-br from-yellow-400 via-red-500 to-purple-600 p-1">
+                            <Skeleton className="w-full h-full rounded-full bg-white" />
+                            <div className="absolute bottom-6 right-4 w-8 h-8 bg-red-500  rounded-full flex items-center justify-center border-2 border-white">
+                                <Skeleton className="w-4 h-4 rounded-full" />
+                            </div>
                         </div>
                     </div>
                 </div>
