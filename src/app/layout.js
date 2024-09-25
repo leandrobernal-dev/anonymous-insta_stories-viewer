b3240daad9881167bde69f399700512b87421c92
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-25 13:38:19 +0800

    Update background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary`}
                 suppressHydrationWarning
             >
                 {children}
