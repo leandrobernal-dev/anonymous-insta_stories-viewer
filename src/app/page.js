fb58a28f86c1237f71074d71f77d9486b4b9d32e
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-26 17:29:36 +0800

    Add loading when entered username

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,15 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { InstagramLogoIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export default function Home() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
     const router = useRouter();
 
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevent the default form submission
+        setIsSubmitted(true);
         const username = event.target.username.value; // Get the username value
         if (username) {
             router.push(`/${username}`); // Redirect to the new URL
@@ -34,6 +37,7 @@ export default function Home() {
                             type="text"
                             name="username"
                             id="username"
+                            required
                             placeholder="Enter Instagram username"
                             className="w-full bg-zinc-800 border-zinc-700 text-white placeholder-zinc-500 focus:border-white focus:ring-white"
                         />
@@ -42,6 +46,28 @@ export default function Home() {
                         type="submit"
                         className="w-full bg-white text-black hover:bg-zinc-200 transition-colors"
                     >
+                        {isSubmitted && (
+                            <svg
+                                class="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    class="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    stroke-width="4"
+                                ></circle>
+                                <path
+                                    class="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                            </svg>
+                        )}
                         View Stories
                     </Button>
                 </form>
