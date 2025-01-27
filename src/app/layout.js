f05108d2a2d0de6f88a57cc4908e7aa7e70e4776
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2025-01-27 10:03:17 +0800

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
     variable: "--font-geist-sans",
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
                 className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary`}
                 suppressHydrationWarning
             >
+                <Analytics />
                 {children}
             </body>
         </html>
